2c99870094fb8b6b6c7105f5f0167cf386003d0d
gahyun <s2112@e-mirim.hs.kr>
2023-05-10 12:57:26 +0900

    초기 커밋.

---
+++ ClassBasic31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    internal class Program234
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        class ThridClass
+        {
+
+        }
+    }
+
+    class SecondClass
+    {
+
+    }
+}
